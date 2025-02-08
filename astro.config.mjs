802ca795fed05049593bc95a0463b7c6c398e7b4
Corentin <cochaux.c@hotmail.fr>
2025-02-08 22:02:34 +0100

    fix: github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://korwen3.github.io'
+    site: 'https://korwen3.github.io',
+    base: '/MaisonSabotage',
 });
